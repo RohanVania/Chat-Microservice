f9dee09580930afc2f24b33457ff274d15783c40
RohanClaclaws <rohan@claclaws.com>
2024-02-13 19:08:02 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CLog } from './helper/AppHelper';
 import cors from 'cors';
 import routes from './routes';
 import { PrismaClient } from '@prisma/client';
+import {initializingSocketServerOnThisServer} from "./socketServer"
 
 if(!process.env.HTTP_PORT) {
     require('dotenv-flow').config()
@@ -20,15 +21,19 @@ const SERVER_PORT = process.env.HTTP_PORT;
 const app = express()
 app.disable('x-powered-by')
 
+
 app.use(express.json())
 
 app.use('*', cors())
 
 app.use('/', routes)
 
+
 const server = app.listen(SERVER_PORT, () => {
     CLog.ok(`NODE_ENV is : ${process.env.NODE_ENV}.\n Express server has started on port ${SERVER_PORT}.`)
 })
+
+initializingSocketServerOnThisServer(server)
 
 server.on('error', (err) => {
     CLog.bad(`Express server encountered an error: ${err}`);
---
+++ src/socketServer.ts
@@ -0,0 +1,24 @@
+import { Server } from "socket.io";
+import { Server as HttpServer } from 'http';
+
+let expressServer: HttpServer | undefined;
+
+//** Initializing the Socket Server */
+export function initializingSocketServerOnThisServer(server: HttpServer) {
+    expressServer = server;
+}
+
+//**Configuring the Socket Server */
+const io = new Server(expressServer, {
+    cors: {
+        origin: "*"
+    }
+});
+
+
+//* This is for main namespace
+io.on('connection',(socket)=>{
+    console.log("Client Connected to our Socket Server")
+})
+
+// Comment Testing
