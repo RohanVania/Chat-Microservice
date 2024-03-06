89e08ea5db029cddc2931b03ceb58a84471718f6
RohanClaclaws <rohan@claclaws.com>
2024-03-06 18:33:36 -0500

    environments test

--- src/socketServer.ts
+++ src/socketServer.ts
@@ -1,5 +1,6 @@
 import { Server } from "socket.io";
 import { Server as HttpServer } from 'http';
+import { disconnect } from "process";
 
 let expressServer: HttpServer | undefined;
 
@@ -17,7 +18,17 @@ export function initializingSocketServerOnThisServer(server: HttpServer) {
     //* This is for main namespace
     io.on('connection', (socket) => {
         console.log("Client Connected to our Socket Server",socket.id)
+        console.log(socket.handshake)
+        socket.emit("hello","hi i am from server")
+    
+        
+    //* Disconnection socket
+        socket.on("disconnect",(reason)=>{
+            console.log(reason)
+        })
     })
+
+    
 
 }
 
